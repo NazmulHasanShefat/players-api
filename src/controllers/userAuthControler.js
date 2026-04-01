f88a81c9494793b09e0126c114494f214bb2f45b
NazmulHasanShefat <119728546+NazmulHasanShefat@users.noreply.github.com>
2026-04-01 13:21:02 +0600

    config controller

--- src/controllers/userAuthControler.js
+++ src/controllers/userAuthControler.js
@@ -27,7 +27,7 @@ export const register = async (req, res) => {
         res.cookie("token", token, {
             httpOnly: true,
             secure: process.env.NODE_ENV === "production",
-            sameSite: process.env.NODE_ENV === "production" ? "node" : "strict",
+            sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
             maxAge: 1 * 24 * 60 * 60 * 1000
         })
 
@@ -59,7 +59,7 @@ export const login = async (req, res) => {
         res.cookie("token", token, {
             httpOnly: true,
             secure: process.env.NODE_ENV === "production",
-            sameSite: process.env.NODE_ENV === "production" ? "node" : "strict",
+            sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
             maxAge: 1 * 24 * 60 * 60 * 1000
         });
 
